8de359dd3ae226065f89861a6f7d1bb03bc8bfde
devwesolowski <dev.wesolowski@gmail.com>
2023-03-26 19:29:27 -0500

    changed details a bit, now includes sunrise sunset. got all basic api calls injected

--- src/index.js
+++ src/index.js
@@ -24,7 +24,24 @@ function getWeather() {
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("location").textContent = data.name + " Weather";
-      //   document.getElementById("current-temp").textContent = "";
+      document.getElementById("current-temp").textContent =
+        Math.floor(data.main.temp) + "°";
+      document.getElementById("feels-like-temp").textContent =
+        "Feels Like " + Math.floor(data.main.feels_like) + "°";
+      //TODO
+      //just noticed that the daily min/max seems to be for a range within a
+      //certain timeframe? actual min is 49.. may need to pull from forecast
+      document.getElementById("min-temp").textContent =
+        Math.floor(data.main.temp_min) + "°";
+      document.getElementById("max-temp").textContent =
+        Math.floor(data.main.temp_max) + "°";
+      //TODO
+      //write function to decide image based on different descriptions
+      document.getElementById("humidity").textContent = data.main.humidity;
+      document.getElementById("wind").textContent = data.wind.speed;
+      console.log(getSun(data.sys.sunrise));
+      document.getElementById("sunrise").textContent = getSun(data.sys.sunrise);
+      document.getElementById("sunset").textContent = getSun(data.sys.sunset);
     })
     .catch((error) => console.error(error));
 }
@@ -37,4 +54,11 @@ function requestForecast() {
       //
     })
     .catch((error) => console.error(error));
+}
+
+function getSun(sun) {
+  const time = new Date(sun * 1000).toLocaleTimeString("en-US", {
+    timeStyle: "short",
+  });
+  return time;
 }
